5aa20a3ef5d2388a314d0f4ec18ed368f6270d22
dchild <dchild@LT13>
2013-09-21 11:04:22 -0600

    first pass at the server

---
+++ blockfilt/src/org/ogs/bf/server/MyServer.java
@@ -0,0 +1,16 @@
+package org.ogs.bf.server;
+
+import org.restlet.Application;
+import org.restlet.Component;
+import org.restlet.data.Protocol;
+
+public class MyServer extends Application {
+
+	public static void main(String[] args) throws Exception {
+		System.out.println("starting server .... ");
+		Component component = new Component();
+		component.getServers().add(Protocol.HTTP, 80);
+		component.getDefaultHost().attach("/run", TestResource.class);
+		component.start();
+	}
+}
